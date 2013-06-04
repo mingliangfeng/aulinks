6cd43790970e811e24c380efaa017dc82f5cbbb2
Andrew Feng <mingliangfeng@gmail.com>
2013-06-04 11:21:45 +1000

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aulinks::Application.config.secret_token = '25f5d1b7c21a75880df56a35bfe709a31736fcab94f97737c826c02c6a4c37d068fa32164b6fcf335414ae720b498dfff0b0334ad6463bd94ba78fbdf6c81ed9'
